5081198c3c3c43c772d1fdfb31d72fdafd6c5d35
Roy Evangelista <royevangelista@gmail.com>
2018-03-17 19:59:30 +0800

    Update stringutil

--- stringutil/stringutil.go
+++ stringutil/stringutil.go
@@ -4,8 +4,9 @@ import (
 	"sort"
 )
 
-func Contains(hayStack []string, target string) bool {
-	sort.Strings(hayStack)
-	i := sort.SearchStrings(hayStack, target)
-	return i < len(hayStack) && hayStack[i] == target
+// Contains check if a string is present in a slice.
+func Contains(l []string, s string) bool {
+	sort.Strings(l)
+	i := sort.SearchStrings(l, s)
+	return i < len(l) && l[i] == s
 }
--- stringutil/stringutil_test.go
+++ stringutil/stringutil_test.go
@@ -1,1 +1,45 @@
 package stringutil
+
+import "testing"
+
+func TestContains(t *testing.T) {
+	l := []string{"abc", "def", "123", "456"}
+	tt := []struct {
+		s     string
+		found bool
+	}{
+		{
+			s:     "abc",
+			found: true,
+		},
+		{
+			s:     "abcd",
+			found: false,
+		},
+		{
+			s:     "def",
+			found: true,
+		},
+		{
+			s:     "123",
+			found: true,
+		},
+		{
+			s:     "1",
+			found: false,
+		},
+		{
+			s:     "456",
+			found: true,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.s, func(t *testing.T) {
+			found := Contains(l, tc.s)
+			if found != tc.found {
+				t.Fatalf("expected found to %v, got %v", tc.found, found)
+			}
+		})
+	}
+}
